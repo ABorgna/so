8d0bd0ebea003af9f72ae64e299a32d41d67e01f
mgatti <mathigatti@gmail.com>
2016-10-16 12:29:17 -0300

    Informe, fatan un par de cosillas

--- tp2/backend/locks/test.cpp
+++ tp2/backend/locks/test.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define  CANT_THREADS  5
+
 #define test(fn)                           \
     do {                                   \
         cout << "running " << #fn << endl; \
@@ -20,6 +22,38 @@ class RWLockTester : public RWLock {
     int writersLeft() { return writers; }
     int readersLeft() { return reading; }
 };
+
+int variable_global = 0;
+
+void *pTest(void *p_lock) {
+    RWLockTester lock = *((RWLockTester*) p_lock);
+    assert(not lock.isLocked());
+
+    lock.rlock();
+
+    assert(lock.isLocked());
+    assert(lock.readersLeft() == 1);
+    cout << "Reading! "  << variable_global << endl;
+
+    lock.runlock();
+
+    assert(not lock.isLocked());
+
+    lock.wlock();
+
+    assert(lock.isWriting());
+    assert(lock.writersLeft() == 1);
+    variable_global++;
+    variable_global--;
+    cout << "Writing! " << variable_global << endl;
+
+    lock.wunlock();
+
+    assert(not lock.isLocked());
+
+    return NULL;
+
+}
 
 void test_basic() {
     RWLockTester lock;
@@ -50,9 +84,31 @@ void test_basic() {
 }
 
 int main() {
+
+    // Test Basico
+
     test(test_basic);
 
     cout << "done!" << endl;
+
+	// Test Multi Threads
+
+    RWLockTester lock;
+	variable_global = 0;
+	cout << "Empezamos! " << variable_global << endl;
+
+    pthread_t thread[CANT_THREADS];
+    int tid;
+
+    for (tid = 0; tid < CANT_THREADS; ++tid) {
+         pthread_create(&thread[tid], NULL, pTest, &lock);
+    }
+
+    for (tid = 0; tid < CANT_THREADS; ++tid)
+         pthread_join(thread[tid], NULL);
+
+    cout << "Terminamos! " << variable_global << endl;
+
 
     return 0;
 }
